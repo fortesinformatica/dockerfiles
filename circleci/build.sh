342e5552643ba06011c3989d39774e8493611ad3
Tiago Moraes <tiagoefmoraes@gmail.com>
2021-02-08 15:01:59 -0300

    fortes/circleci:2021-02_ruby_2.6.6

--- circleci/build.sh
+++ circleci/build.sh
@@ -5,4 +5,4 @@ set -x
 
 docker build -t="fortes/circleci:building" .
 docker run --rm -v "$PWD:/test" "fortes/circleci:building" bash /test/test.sh
-docker tag "fortes/circleci:building" "fortes/circleci:2021-01_node_14_b"
+docker tag "fortes/circleci:building" "fortes/circleci:2021-02_ruby_2.6.6"
