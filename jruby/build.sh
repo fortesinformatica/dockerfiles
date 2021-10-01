270924b3540e140a49de316222c4055a525d101a
Tiago Moraes <tiagoefmoraes@gmail.com>
2021-10-01 14:34:38 -0300

    rubygems 3.2.15 e bundler 2.2.15

--- jruby/build.sh
+++ jruby/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker build -t="fortes/jruby:9.2.14.0_bundler_2.2.8" .
+docker build -t="fortes/jruby:9.2.14.0_bundler_2.2.15" .
--- ruby/build.sh
+++ ruby/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker build -t="fortes/ruby:2.7.2" .
+docker build -t="fortes/ruby:2.7.2_bundler_2.2.15" .
