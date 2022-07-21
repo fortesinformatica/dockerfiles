cf1259aef63a053406f099bab63d76dd83f1464c
Thiago Nogueira <thonogueira@gmail.com>
2022-07-21 18:32:44 -0300

    node update

--- circleci/build.sh
+++ circleci/build.sh
@@ -5,4 +5,4 @@ set -x
 
 docker build -t="fortes/circleci:building" .
 docker run --rm -v "$PWD:/test" "fortes/circleci:building" bash /test/test.sh
-docker tag "fortes/circleci:building" "fortes/circleci:2021-12"
+docker tag "fortes/circleci:building" "fortes/circleci:2022-07"
