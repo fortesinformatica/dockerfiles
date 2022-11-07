f8748232f43b02f5d53163a592986d336f448019
Clavius Tales <clavius.tales@gmail.com>
2022-11-07 14:40:00 -0300

    node update

--- circleci/build.sh
+++ circleci/build.sh
@@ -5,4 +5,4 @@ set -x
 
 docker build -t="fortes/circleci:building" .
 docker run --rm -v "$PWD:/test" "fortes/circleci:building" bash /test/test.sh
-docker tag "fortes/circleci:building" "fortes/circleci:2022-10_2"
+docker tag "fortes/circleci:building" "fortes/circleci:2022-11"
