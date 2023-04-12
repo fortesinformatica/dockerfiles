65c0535b926f60ac99b0df6bb34a61c1b55b47f0
Clavius Tales <clavius.tales@gmail.com>
2023-04-12 17:12:38 -0300

    Node upgrade

--- circleci/build.sh
+++ circleci/build.sh
@@ -5,4 +5,4 @@ set -x
 
 docker build -t="fortes/circleci:building" .
 docker run --rm -v "$PWD:/test" "fortes/circleci:building" bash /test/test.sh
-docker tag "fortes/circleci:building" "fortes/circleci:2023-02" # Ruby 3.2.2
+docker tag "fortes/circleci:building" "fortes/circleci:2023-04" # Node 16.20.0
