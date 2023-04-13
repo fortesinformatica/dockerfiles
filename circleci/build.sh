2c549d43846489ffca05f0c7cbfda73ad44bca39
Clavius Tales <clavius.tales@gmail.com>
2023-04-12 21:29:03 -0300

    RubyGems out of date

--- circleci/build.sh
+++ circleci/build.sh
@@ -5,4 +5,4 @@ set -x
 
 docker build -t="fortes/circleci:building" .
 docker run --rm -v "$PWD:/test" "fortes/circleci:building" bash /test/test.sh
-docker tag "fortes/circleci:building" "fortes/circleci:2023-04" # Node 16.20.0
+docker tag "fortes/circleci:building" "fortes/circleci:2023-04_1" # RubyGems out of date
